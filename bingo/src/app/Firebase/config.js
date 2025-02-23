42d56b15276b1c1e3233d24e25d07e82a5e75708
Bing6000s <bingtrio@outlook.com>
2025-02-23 13:41:17 -0500

    finished the sign in and login

--- bingo/src/app/Firebase/config.js
+++ bingo/src/app/Firebase/config.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBeQpREnvEJpAIIgglsRO--zlvDzVh6tXg",
   authDomain: "bingo-57821.firebaseapp.com",
@@ -15,5 +11,6 @@ const firebaseConfig = {
   appId: "1:1065660189615:web:ecab8b24b6571ad32bbd5a",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp;
+const auth = getAuth(app)
+export {app, auth}
